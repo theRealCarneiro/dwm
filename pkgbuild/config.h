e205d90c5a1471352306252d0e006adfa3311c3e
theRealCarneiro <gabriel.chaves.carneiro@gmail.com>
2021-02-24 22:23:50 -0300

    added pkgbuild

--- pkgbuild/config.h
+++ pkgbuild/config.h
@@ -45,11 +45,11 @@ static const Rule rules[] = {
 	 *	WM_CLASS(STRING) = instance, class
 	 *	WM_NAME(STRING) = title
 	 */
-	/* class     instance  title           tags mask  isfloating  isterminal  noswallow  monitor */
-	{ NULL,		NULL,     "Event Tester", 0,         0,          0,           1,        -1 }, //xev 
-	{ "scratchpad",NULL,     NULL,           0,         1,          1,          -1,        -1 },
-	{ "Alacritty",	NULL,     NULL,           0,         0,          1,          -1,        -1 },
-	{ "St",		NULL,	NULL,	      0,         0,          1,          -1,        -1 }, 
+	/* class     instance  title           tags mask  isfloating  monitor */
+	{ NULL,		NULL,     "Event Tester", 0,         0,          -1 }, //xev 
+	{ "scratchpad",NULL,     NULL,           0,         1,          -1 },
+	{ "Alacritty",	NULL,     NULL,           0,         0,          -1 },
+	{ "St",		NULL,	NULL,	      0,         0,          -1 }, 
 };
 
 /* layout(s) */
